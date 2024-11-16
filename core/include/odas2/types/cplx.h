42ba60460f918560dc9b32476f452da3641a52bd
Marc-Antoine Maheux <marc-antoine.maheux@usherbrooke.ca>
2024-11-15 21:38:29 -0500

    Optimization.

--- core/include/odas2/types/cplx.h
+++ core/include/odas2/types/cplx.h
@@ -1,6 +1,8 @@
 #ifndef __CPLX_H
 #define __CPLX_H
 
+#include <math.h>
+
 typedef struct cplx_t {
 
     float real;
@@ -8,20 +10,103 @@ typedef struct cplx_t {
 
 } cplx_t;
 
-cplx_t cplx_cst(const float real, const float imag);
+inline cplx_t cplx_cst(const float real, const float imag) {
 
-cplx_t cplx_add(const cplx_t op1, const cplx_t op2);
+    cplx_t rtn;
 
-cplx_t cplx_sub(const cplx_t op1, const cplx_t op2);
+    // (a + jb)
+    rtn.real = real;
+    rtn.imag = imag;
 
-cplx_t cplx_mul(const cplx_t op1, const cplx_t op2);
+    return rtn;
 
-cplx_t cplx_div(const cplx_t op1, const cplx_t op2);
+}
 
-cplx_t cplx_conj(const cplx_t op);
+inline cplx_t cplx_add(const cplx_t op1, const cplx_t op2) {
 
-cplx_t cplx_unit(const cplx_t op);
+    cplx_t rtn;
 
-float cplx_l2(const cplx_t op);
+    // (a + jb) + (c + jd) = (a + c) + j(b + d)
+    rtn.real = op1.real + op2.real;
+    rtn.imag = op1.imag + op2.imag;
+
+    return rtn;
+
+}
+
+inline cplx_t cplx_sub(const cplx_t op1, const cplx_t op2) {
+
+    cplx_t rtn;
+
+    // (a + jb) - (c + jd) = (a - c) + j(b - d)
+    rtn.real = op1.real - op2.real;
+    rtn.imag = op1.imag - op2.imag;
+
+    return rtn;
+
+}
+
+inline cplx_t cplx_mul(const cplx_t op1, const cplx_t op2) {
+
+    cplx_t rtn;
+
+    // (a + jb) * (c + jd) = (ac - bd) + j(bc + ad)
+    rtn.real = op1.real * op2.real - op1.imag * op2.imag;
+    rtn.imag = op1.real * op2.imag + op2.real * op1.imag;
+
+    return rtn;
+
+}
+
+inline cplx_t cplx_div(const cplx_t op1, const cplx_t op2) {
+
+    cplx_t rtn;
+
+    // (a + jb) / (c + jd) = (a + jb) * (c - jd) / [(c + jd) * (c - jd)]
+    //                     = [(ac + bd) + j(bc - ad)] / [c^2 + d^2]
+    //                     = [(ac + bd) / (c^2 + d^2)] + j[(bc - ad) / (c^2 + d^2)]
+    rtn.real = (op1.real * op2.real + op1.imag * op2.imag) / (op2.real * op2.real + op2.imag * op2.imag);
+    rtn.imag = (op1.imag * op2.real - op1.real * op2.imag) / (op2.real * op2.real + op2.imag * op2.imag);
+
+    return rtn;
+
+}
+
+inline cplx_t cplx_conj(const cplx_t op) {
+
+    cplx_t rtn;
+
+    // (a + jb)' = a - jb
+    rtn.real = op.real;
+    rtn.imag = -1.0f * op.imag;
+
+    return rtn;
+
+}
+
+inline cplx_t cplx_unit(const cplx_t op) {
+
+    cplx_t rtn;
+    const float eps = 1e-20;
+
+    // (a + jb) / ((a^2 + b^2)^0.5 + eps) = a / ((a^2 + b^2)^0.5 + eps) + j b / ((a^2 + b^2)^0.5 + eps)
+    float magnitude = sqrtf(op.real * op.real + op.imag * op.imag);
+    rtn.real = op.real / (magnitude + eps);
+    rtn.imag = op.imag / (magnitude + eps);
+
+    return rtn;
+
+}
+
+inline float cplx_l2(const cplx_t op) {
+
+    float rtn;
+
+    // |a + jb|^2 = a^2 + b^2
+    rtn = op.real * op.real + op.imag * op.imag;
+
+    return rtn;
+
+}
 
 #endif // __CPLX_H
--- core/include/odas2/types/xyz.h
+++ core/include/odas2/types/xyz.h
@@ -1,6 +1,9 @@
 #ifndef __XYZ_H
 #define __XYZ_H
 
+#include <math.h>
+#include <stdio.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,25 +16,128 @@ typedef struct xyz_t {
 
 } xyz_t;
 
-xyz_t xyz_cst(const float x, const float y, const float z);
+inline xyz_t xyz_cst(const float x, const float y, const float z) {
 
-xyz_t xyz_unit(const xyz_t op);
+    xyz_t rtn;
 
-float xyz_mag(const xyz_t op);
+    rtn.x = x;
+    rtn.y = y;
+    rtn.z = z;
 
-float xyz_l2(const xyz_t op);
+    return rtn;
 
-float xyz_dot(const xyz_t op1, const xyz_t op2);
+}
 
-xyz_t xyz_vec(const xyz_t op1, const xyz_t op2);
+inline float xyz_mag(const xyz_t op) {
 
-xyz_t xyz_add(const xyz_t op1, const xyz_t op2);
+    float rtn;
 
-xyz_t xyz_sub(const xyz_t op1, const xyz_t op2);
+    // (x^2 + y^2 + z^2)^0.5
+    rtn = sqrtf(op.x * op.x + op.y * op.y + op.z * op.z);
 
-xyz_t xyz_scale(const xyz_t op, const float scale);
+    return rtn;
 
-void xyz_printf(const xyz_t op);
+}
+
+inline xyz_t xyz_unit(const xyz_t op) {
+
+    xyz_t rtn;
+    float mag;
+
+    mag = xyz_mag(op);
+
+    // x' = x / (x^2 + y^2 + z^2)^0.5
+    // y' = y / (x^2 + y^2 + z^2)^0.5
+    // z' = z / (x^2 + y^2 + z^2)^0.5
+    // Added epsilon to avoid division by 0
+    rtn.x = op.x / (mag + 1e-20);
+    rtn.y = op.y / (mag + 1e-20);
+    rtn.z = op.z / (mag + 1e-20);
+
+    return rtn;
+
+}
+
+inline float xyz_l2(const xyz_t op) {
+
+    float rtn;
+
+    // (x^2 + y^2 + z^2)
+    rtn = op.x * op.x + op.y * op.y + op.z * op.z;
+
+    return rtn;
+
+}
+
+inline float xyz_dot(const xyz_t op1, const xyz_t op2) {
+
+    float rtn;
+
+    // Dot product
+    // x1 * x2 + y1 * y2 + z1 * z2
+    rtn = op1.x * op2.x + op1.y * op2.y + op1.z * op2.z;
+
+    return rtn;
+
+}
+
+inline xyz_t xyz_vec(const xyz_t op1, const xyz_t op2) {
+
+    xyz_t rtn;
+
+    // Cross product
+    // x' = (y1 * z2 - z1 * y2)
+    // y' = (z1 * x2 - x1 * z2)
+    // z' = (x1 * y2 - y1 * x2)
+    rtn.x = op1.y * op2.z - op1.z * op2.y;
+    rtn.y = op1.z * op2.x - op1.x * op2.z;
+    rtn.z = op1.x * op2.y - op1.y * op2.x;
+
+    return rtn;
+
+}
+
+inline xyz_t xyz_add(const xyz_t op1, const xyz_t op2) {
+
+    xyz_t rtn;
+
+    rtn.x = op1.x + op2.x;
+    rtn.y = op1.y + op2.y;
+    rtn.z = op1.z + op2.z;
+
+    return rtn;
+
+}
+
+inline xyz_t xyz_sub(const xyz_t op1, const xyz_t op2) {
+
+    xyz_t rtn;
+
+    rtn.x = op1.x - op2.x;
+    rtn.y = op1.y - op2.y;
+    rtn.z = op1.z - op2.z;
+
+    return rtn;
+
+}
+
+inline xyz_t xyz_scale(const xyz_t op, const float scale) {
+
+    xyz_t rtn;
+
+    rtn.x = op.x * scale;
+    rtn.y = op.y * scale;
+    rtn.z = op.z * scale;
+
+    return rtn;
+
+}
+
+inline void xyz_printf(const xyz_t op) {
+
+    printf("(%+1.3f, %+1.3f, %+1.3f)\n", op.x, op.y, op.z);
+
+}
 
 #ifdef __cplusplus
 } //extern "C"
