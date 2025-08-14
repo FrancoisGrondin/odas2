1e56b5486c2ad8eb918a4981d301f206c1695d57
Marc-Antoine Maheux <marc-antoine.maheux@darit.tech>
2025-08-14 11:47:57 -0400

    Fix build for MSVC

--- core/src/utils/openmp.c
+++ core/src/utils/openmp.c
@@ -5,7 +5,7 @@
 
 #define MAX_THREAD_COUNT 4
 
-inline uint16_t get_thread_count(void)
+uint16_t get_thread_count(void)
 {
     static int initialized = 0;
     static uint16_t thread_count = 0;
@@ -23,12 +23,12 @@ inline uint16_t get_thread_count(void)
 
 #else
 
-inline uint16_t get_thread_count(void)
+uint16_t get_thread_count(void)
 {
     return 1;
 }
 
-inline int omp_get_thread_num(void)
+int omp_get_thread_num(void)
 {
     return 0;
 }
